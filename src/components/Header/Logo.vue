e0d9c30e64a033aa257cebb7cd1b565c5c4ff806
igorbezsmertnyi <bezsmertnyi.igor@gmail.com>
2018-07-07 19:00:08 +0200

    instal elements-ui

---
+++ src/components/Header/Logo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="logo">
+    <img src="@/assets/js-logo.png" alt="Js Workshop Logo">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Logo'
+}
+</script>
+
+<style lang="stylus">
+
+</style>
---
+++ src/components/Header/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header">
+    <logo />
+  </header>
+</template>
+
+<script>
+import Logo from './Logo'
+
+export default {
+  name: 'AppHeader',
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="stylus">
+
+</style>
