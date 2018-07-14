52ebc4c6adecc5f87598261cfa30279bb73cf387
igorbezsmertnyi <bezsmertnyi.igor@gmail.com>
2018-07-14 10:36:15 +0200

    update number page

--- src/Pages/Number.vue
+++ src/Pages/Number.vue
@@ -6,56 +6,54 @@
 
     <div class="tutorial__body">
       <p>
-        Numbers are values that can be used in mathematical operations. You don’t need any special syntax 
-        for numbers — just write them straight into JavaScript.
+        JavaScript has only one type of number - this is a double-precision floating-point number. 
+        This cause that the results of the calculation of some expressions are arithmetically incorrect.
+        Maybe you know that the result of <code>0.1 + 0.2</code> doesn’t a <code>0.3</code>. But if you 
+        work with integer this problem doesn’t happen.
       </p>
+
+      <p>
+        JS has <mark>Number</mark> object, it represents an object wrapper for numeric values. The Object of type
+        Number can be created by using command <code>const a = new Number(10)</code> or you can rely on automatically 
+        behaver of JS described above. That allow you to execute methods contains in <code>Number.prototype</code>
+        and apply them to the number literals.
+      </p>
+
+      <br/>
 
       <VEmbed class="tutorial__gist" :id="gists[0]" :options="{ emoji: true }">
-        https://gist.github.com/igorbezsmertnyi/b3d5c3cdadf453f197fbc2da5c45df21
+        https://gist.github.com/igorbezsmertnyi/cb34f098b7e958cb0af2cc372302d253
       </VEmbed>
 
-      <h3>Decimals and fractions</h3>
+      <br/>
+
       <p>
-        JavaScript doesn’t distinguish between whole numbers and decimals, so you can use them together without 
-        having to convert from one to the other.
+        There are global functions for transform values of other types to a number type. This is - <mark>parseInt()</mark>,
+        <mark>parseFloat()</mark> and constructor <mark>Number()</mark>, which in this case used like regular function 
+        which make transformation.
       </p>
 
+      <br/>
+
       <VEmbed class="tutorial__gist" :id="gists[1]" :options="{ emoji: true }">
-        https://gist.github.com/igorbezsmertnyi/f0fbb440d7d5aa84e2411f511f7e94f8
+        https://gist.github.com/igorbezsmertnyi/d027d4f33021206f6c845dd25840bcf9
       </VEmbed>
 
+      <br/>
+
       <p>
-        <mark>Fractions</mark> don’t exist in JavaScript, buy you can rewrite them as division problems using 
-        the division operator /. Note that the resulting number is always converted to decimals — just like with a calculator.
+        If, during the operation with numbers, something is produced that is not a number (during some calculation or trying 
+        transformation to number) JavaScript doesn’t give an error, but the result of such an operation as the value of NaN
+        (Not-a-Number). For check, if some value is NaN you can use <code>isNaN()</code> function.
+      </p>
+
+      <p> 
+        The arithmetic operation in JS works quite the usual way, but we must pay attention to the fact that the "+" operator 
+        can perform addition of numbers and concatenation of strings.
       </p>
 
       <VEmbed class="tutorial__gist" :id="gists[2]" :options="{ emoji: true }">
-        https://gist.github.com/igorbezsmertnyi/abccded6f03fa8a0e46e58ab0fa89b52
-      </VEmbed>
-
-      <p>Improper fractions use the division operator in the same way.</p>
-
-      <VEmbed class="tutorial__gist" :id="gists[3]" :options="{ emoji: true }">
-        https://gist.github.com/igorbezsmertnyi/e2e56fd83fd720f88d4afbcdc140fcae
-      </VEmbed>
-
-      <p>To use mixed numbers, you need to add the whole number and fraction.</p>
-
-      <VEmbed class="tutorial__gist" :id="gists[4]" :options="{ emoji: true }">
-        https://gist.github.com/igorbezsmertnyi/a2ae4487fb0d115a3af841ea9a0ef8f2
-      </VEmbed>
-
-      <h3>Negative numbers</h3>
-      <p>You can make a number negative by placing the - operator in front.</p>
-
-      <VEmbed class="tutorial__gist" :id="gists[5]" :options="{ emoji: true }">
-        https://gist.github.com/igorbezsmertnyi/ac73939dc8846ab7ab6d3bd49d9422f1
-      </VEmbed>
-
-      <p>You can also get a negative number by subtracting a number from a smaller number.</p>
-
-      <VEmbed class="tutorial__gist" :id="gists[6]" :options="{ emoji: true }">
-        https://gist.github.com/igorbezsmertnyi/f1108b80f9a491d6cec25943efbdc29c
+        https://gist.github.com/igorbezsmertnyi/caa0e5ae05d2a3d46d85030ca6d9eb80
       </VEmbed>
     </div>
   </div>
@@ -72,14 +70,9 @@ export default {
 
   data: () => ({
     gists: [
-      'b3d5c3cdadf453f197fbc2da5c45df21',
-      'f0fbb440d7d5aa84e2411f511f7e94f8',
-      'abccded6f03fa8a0e46e58ab0fa89b52',
-      'e2e56fd83fd720f88d4afbcdc140fcae',
-      'a2ae4487fb0d115a3af841ea9a0ef8f2',
-      'ac73939dc8846ab7ab6d3bd49d9422f1',
-      'f1108b80f9a491d6cec25943efbdc29c',
-      'd51ee20f7499da7e01ec875edca6632b'
+      'cb34f098b7e958cb0af2cc372302d253',
+      'd027d4f33021206f6c845dd25840bcf9',
+      'caa0e5ae05d2a3d46d85030ca6d9eb80'
     ]
   })
 }
