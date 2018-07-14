5b93f77a6db773968880221dadcca3cf1c3ddca9
igorbezsmertnyi <bezsmertnyi.igor@gmail.com>
2018-07-14 11:01:41 +0200

    update boolan page

--- src/Pages/Boolean.vue
+++ src/Pages/Boolean.vue
@@ -6,13 +6,17 @@
 
     <div class="tutorial__body">
       <p>
-        <mark>Booleans</mark> are values that can be only one of two things: true or false. Anything “on” or “off,” 
-        “yes” or “no,” or temporary is a usually good fit for a boolean. It’s useful to store booleans in variables 
-        to keep track of their values and change them over time.
+        The Logical data type in JS is represented by two values - <code>true</code> and <code>false</code>. The language
+        can automatically transform different values to the boolean type. So false in addition to the 
+        logical value false is <code>null</code>, <code>undefined</code>, <code>''</code>(empty string), <code>0</code>
+        and <code>NaN</code>. The other values including any others object are the true value. During logical operation
+        execution, everything that is true transform to <code>true</code> and everything that is false transform 
+        to <code>false</code>. In according to the above principles the empty string will be transformed to <code>false</code>
+        and in a result of following code execution the console receives a string <code>This is false</code>
       </p>
 
-      <VEmbed class="tutorial__gist" :id="gists[1]" :options="{ emoji: true }">
-        https://gist.github.com/igorbezsmertnyi/ecea9c6a751f266aa0225e26887c1bc4
+      <VEmbed class="tutorial__gist" :id="gists[0]" :options="{ emoji: true }">
+        https://gist.github.com/igorbezsmertnyi/eac2f66bf0c0360a8091893cb550e6d8
       </VEmbed>
     </div>
   </div>
@@ -29,7 +33,7 @@ export default {
 
   data: () => ({
     gists: [
-      'ecea9c6a751f266aa0225e26887c1bc4'
+      'eac2f66bf0c0360a8091893cb550e6d8'
     ]
   })
 }
