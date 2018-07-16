afe59abcd2cf8f6b5ce93a70df0f4a8e1580d3a1
igorbezsmertnyi <bezsmertnyi.igor@gmail.com>
2018-07-16 18:11:54 +0200

    update styles

--- src/Pages/Introduction.vue
+++ src/Pages/Introduction.vue
@@ -12,13 +12,9 @@
         In browsers scripts straightly connected to HTML document and initialized at once when the page loaded.
       </p>
 
-      <br />
-
       <p>
         <strong>Programs on JavaScript - regular string. They are not required special preparing before running</strong>
       </p>
-
-      <br />
 
       <h3>Why JavaScript?</h3>
 
@@ -26,13 +22,13 @@
         When was creating JavaScript, it had another name <mark>"LiveScript"</mark>. Then was very popular Java, and marketers decided 
         that the similar name will make the language more popular on start.
       </p>
-      <br />
+
       <p>
         Was planned that JavaScript will be such "small brother" of Java. But history ordered otherwise. But history ordered otherwise, 
         JavaScript fast grew up, and now JavaScript is independent language, with own specification
         <a href="https://en.wikipedia.org/wiki/ECMAScript" target="_blank">(ECMAScript)</a> and Java has nothing to do with it.
       </p>
-      <br />
+    
       <p>
         It has many features which make learning hard but in this course, we will try to explain it
       </p>
